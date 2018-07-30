ec6028e5ba541313a8452ed039b5fc2707a8363c
tdrk1980 <jabberwock_takatuki@yahoo.co.jp>
2018-07-31 03:00:03 +0900

    add SetApplConfig/GetApplConfig

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,15 +6,19 @@ import vxlapi as xl
 
 XL_SUCCESS = 0
 
+XL_HWTYPE_VIRTUAL = 1
+
+XL_BUS_TYPE_CAN = 0x00000001
+
 class Test(unittest.TestCase):
-    def setUp(self):
-        print("setUp")
+    # def setUp(self):
+    #     print("setUp")
 
-    def tearDown(self):
-        print("tearDown")
+    # def tearDown(self):
+    #     print("tearDown")
 
-    def doCleanups(self):
-        print("cleanup")
+    # def doCleanups(self):
+    #     print("cleanup")
 
     def test_open(self):
         print(inspect.getframeinfo(inspect.currentframe())[2])
@@ -23,6 +27,41 @@ class Test(unittest.TestCase):
 
     def test_close(self):
         print(inspect.getframeinfo(inspect.currentframe())[2])
+        ret = xl.CloseDriver()
+        self.assertEqual(ret, XL_SUCCESS)
+    
+    def test_setapplconfig_getapplconfig(self):
+        print(inspect.getframeinfo(inspect.currentframe())[2])
+
+        ret = xl.OpenDriver()
+        self.assertEqual(ret, XL_SUCCESS)
+
+        # CAN1 -- virtual can bus1.hwch1
+        ret = xl.SetApplConfig(appName=bytes("pyxldrv".encode()), appChannel=0, pHwType=[XL_HWTYPE_VIRTUAL], pHwIndex=[0], pHwChannel=[0], busType=XL_BUS_TYPE_CAN)
+        self.assertEqual(ret, XL_SUCCESS)
+
+        pHwType = [0xff]
+        pHwIndex = [0xff]
+        pHwChannel = [0xff]
+        ret = xl.GetApplConfig(appName=bytes("pyxldrv".encode()), appChannel=0, pHwType=pHwType, pHwIndex=pHwIndex, pHwChannel=pHwChannel, busType=XL_BUS_TYPE_CAN)
+        self.assertEqual(ret, XL_SUCCESS)
+        self.assertEqual(pHwType[0], XL_HWTYPE_VIRTUAL)
+        self.assertEqual(pHwIndex[0], 0)
+        self.assertEqual(pHwChannel[0], 0)
+
+        # CAN2 -- virtual can bus1.hwch2
+        ret = xl.SetApplConfig(appName=bytes("pyxldrv".encode()), appChannel=1, pHwType=[XL_HWTYPE_VIRTUAL], pHwIndex=[0], pHwChannel=[1], busType=XL_BUS_TYPE_CAN)
+        self.assertEqual(ret, XL_SUCCESS)
+
+        pHwType = [0xff]
+        pHwIndex = [0xff]
+        pHwChannel = [0xff]
+        ret = xl.GetApplConfig(appName=bytes("pyxldrv".encode()), appChannel=1, pHwType=pHwType, pHwIndex=pHwIndex, pHwChannel=pHwChannel, busType=XL_BUS_TYPE_CAN)
+        self.assertEqual(ret, XL_SUCCESS)
+        self.assertEqual(pHwType[0], XL_HWTYPE_VIRTUAL)
+        self.assertEqual(pHwIndex[0], 0)
+        self.assertEqual(pHwChannel[0], 1)
+
         ret = xl.CloseDriver()
         self.assertEqual(ret, XL_SUCCESS)
 
--- vxlapi.pyx
+++ vxlapi.pyx
@@ -6,9 +6,14 @@ cdef extern from "<windows.h>":
 
 cdef extern from "vxlapi.h":
     ctypedef short XLstatus
+
     XLstatus xlOpenDriver()
     XLstatus xlCloseDriver()
+
     const char *xlGetErrorString(XLstatus err)
+
+    XLstatus xlGetApplConfig(char *appName, unsigned int appChannel, unsigned int *pHwType, unsigned int *pHwIndex, unsigned int *pHwChannel, unsigned int busType)
+    XLstatus xlSetApplConfig(char *appName, unsigned int appChannel, unsigned int hwType, unsigned int hwIndex, unsigned int hwChannel, unsigned int busType)
 
 def OpenDriver():
     return xlOpenDriver()
@@ -19,4 +24,18 @@ def CloseDriver():
 def GetErrorString(XLstatus err):
     return xlGetErrorString(err)
 
+def GetApplConfig(char *appName, unsigned int appChannel, pHwType, pHwIndex, pHwChannel, unsigned int busType):
+    cdef XLstatus status = 0
+    cdef unsigned int hwType    = pHwType[0]
+    cdef unsigned int hwIndex   = pHwIndex[0]
+    cdef unsigned int hwChannel = pHwChannel[0]
 
+    status = xlGetApplConfig(appName, appChannel, &hwType, &hwIndex, &hwChannel, busType)
+    pHwType[0]    = hwType
+    pHwIndex[0]   = hwIndex
+    pHwChannel[0] = hwChannel
+
+    return status
+
+def SetApplConfig(char *appName, unsigned int appChannel, pHwType, pHwIndex, pHwChannel, unsigned int busType):
+    return xlSetApplConfig(appName, appChannel, pHwType[0], pHwIndex[0], pHwChannel[0], busType)
