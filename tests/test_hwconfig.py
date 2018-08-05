55b7f920179f762ac93a0cbd52ef17c3be83b115
tdrk1980 <jabberwock_takatuki@yahoo.co.jp>
2018-08-05 21:51:15 +0900

    use macro

--- tests/test_hwconfig.py
+++ tests/test_hwconfig.py
@@ -1,17 +1,16 @@
 # *-* encoding: utf-8 *-*
-# python -m unittest tests.test_hwconfig
+
 import unittest
 import inspect
 import vxlapi as xl
 
-XL_SUCCESS = 0
-
+# python -m unittest tests.test_hwconfig
 class Test(unittest.TestCase):
     def test_popup(self):
         print(inspect.getframeinfo(inspect.currentframe())[2])
 
         ret = xl.PopupHwConfig()
-        self.assertEqual(ret, XL_SUCCESS)
+        self.assertEqual(ret, xl.XL_SUCCESS)
 
 if __name__ == '__main__':
     unittest.main()
