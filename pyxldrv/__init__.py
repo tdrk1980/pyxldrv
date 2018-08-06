04a5e953c8ba07b56aed069b5399f742f6af02f2
Mgi2 Y.Todoroki MS <Todoroki.Yoshio@ap.MitsubishiElectric.co.jp>
2018-08-06 09:32:45 +0900

    add python code

---
+++ pyxldrv/__init__.py
@@ -0,0 +1,6 @@
+# *-* encoding: utf-8 *-*
+import vxlapi as xl
+import win32event
+# To wait Receive(), WaitForSingleObject is to be used - http://timgolden.me.uk/pywin32-docs/win32event__WaitForSingleObject_meth.html
+
+
